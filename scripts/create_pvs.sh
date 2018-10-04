ad53a293741c18c08deeacb8268cd20e7e94b3a9
uzugic <42067801+uzugic@users.noreply.github.com>
2018-10-04 15:12:54 +0200

    Update create_pvs.sh

--- scripts/create_pvs.sh
+++ scripts/create_pvs.sh
@@ -5,7 +5,7 @@ mkdir -p /srv/nfs/user-vols/pv{1..50}
 echo "Creating PV for users.."
 
 for pvnum in {1..50} ; do
-  echo '/srv/nfs/user-vols/pv${pvnum} *(rw,root_squash)' >> /etc/exports.d/openshift-uservols.exports
+  echo "/srv/nfs/user-vols/pv${pvnum} *(rw,root_squash)" >> /etc/exports.d/openshift-uservols.exports
   chown -R nfsnobody.nfsnobody /srv/nfs
   chmod -R 777 /srv/nfs
 done
