8b417738ae9de5cc6c33c87dd9c7f46e23ffccfe
root <root@bastion.e509.example.opentlc.com>
2018-10-05 13:17:29 +0000

    added script setup file

---
+++ scripts/script_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+sed -i -e 's/\r$//' add-users.sh
+sed -i -e 's/\r$//' create_pvs.sh
+sed -i -e 's/\r$//' generate_inventory.sh
+sed -i -e 's/\r$//' multitenancy.sh
+sed -i -e 's/\r$//' pvs_10Gsize.sh
+sed -i -e 's/\r$//' pvs_5Gsize.sh
+
